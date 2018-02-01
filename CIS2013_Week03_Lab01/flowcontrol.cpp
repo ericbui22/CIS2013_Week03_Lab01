37abb395025bf050dbfb322ab58393ae7b3b4729
Eric Bui <ericbui221@gmail.com>
2018-01-31 20:12:55 -0600

    Add project files.

---
+++ CIS2013_Week03_Lab01/flowcontrol.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char continueProgram = 'x';
+
+	do {
+
+		cout << "Do you want to continue (y/n)" << endl;
+		cin >> continueProgram;
+
+		switch (continueProgram) {
+			case 'y':
+				cout << "Going to continue" << endl;
+				break;
+			case 'n':
+				cout << "Going to exit" << endl;
+				break;
+			default:
+				continueProgram = 'y';
+			}
+	} while (continueProgram == 'y');
+
+	return 0;
+}
