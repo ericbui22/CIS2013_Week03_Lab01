92bbeba02ccb48b984687f4018ac4b155fc6b362
Eric Bui <ericbui221@gmail.com>
2018-01-31 20:32:28 -0600

    changes with the bottles

--- CIS2013_Week03_Lab01/flowcontrol.cpp
+++ CIS2013_Week03_Lab01/flowcontrol.cpp
@@ -4,22 +4,33 @@ using namespace std;
 int main() {
 	char continueProgram = 'x';
 
-	do {
+	for (int i = 99; i >= 0; i = i --) {
+		if (i == 1) {
+			cout << i << "bottle of beer on the wall" << endl;
+		}
+		else if (i == 0) {
+			cout << "No more bottles of beer on the wall" << endl;
+		}
+		else
+			cout << i << "bottles of beer on the wall" << endl;
+	}
 
-		cout << "Do you want to continue (y/n)" << endl;
+	//do {
+
+		//cout << "Do you want to continue (y/n)" << endl;
 		cin >> continueProgram;
 
-		switch (continueProgram) {
-			case 'y':
-				cout << "Going to continue" << endl;
-				break;
-			case 'n':
-				cout << "Going to exit" << endl;
-				break;
-			default:
-				continueProgram = 'y';
-			}
-	} while (continueProgram == 'y');
+		//switch (continueProgram) {
+			//case 'y':
+				//cout << "Going to continue" << endl;
+				//break;
+			//case 'n':
+				//cout << "Going to exit" << endl;
+				//break;
+			//default:
+				//continueProgram = 'y';
+			//}
+	//} while (continueProgram == 'y');
 
 	return 0;
 }
